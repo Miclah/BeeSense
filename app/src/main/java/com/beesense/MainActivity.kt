7aab707537fd44f521cd877ccc25148951f52aa4
Michal Petrán <progamingstudiohd@gmail.com>
2025-03-16 11:00:56 +0100

    MainAcitivity
    - Created MainActivity class as main entry point to the application.
    - Applied theme to the app.
    - Used navigation ui from BottomNavigationBar class.

---
+++ app/src/main/java/com/beesense/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.beesense
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.beesense.ui.components.BottomNavigationBar
+import com.beesense.ui.navigation.Screen
+import com.beesense.ui.screens.GraphScreen
+import com.beesense.ui.theme.BeeSenseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BeeSenseTheme {
+                MainApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainApp() {
+    val navController = rememberNavController()
+    val items = listOf(Screen.Overview, Screen.Graphs)
+    Scaffold(
+        bottomBar = {
+            val navBackStackEntry = navController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry.value?.destination?.route ?: Screen.Overview.route
+            BottomNavigationBar(
+                navController = navController,
+                items = items,
+                currentRoute = currentRoute,
+                onItemSelected = { screen ->
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.startDestinationRoute!!) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Overview.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Graphs.route) { GraphScreen() }
+        }
+    }
+}
+
